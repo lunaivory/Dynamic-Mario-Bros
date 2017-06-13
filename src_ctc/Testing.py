0a51aacc155254765734552666c2ed4e8ee7beb9
Ubuntu <lc@GpuVM.ijfqyjbmupcutanud01fhshsle.ax.internal.cloudapp.net>
2017-06-13 23:09:20 +0000

    add mask to test input, remove print

--- src_ctc/Testing.py
+++ src_ctc/Testing.py
@@ -1,9 +1,13 @@
 import tensorflow as tf
 import numpy as np
-import model
+#import model
 import matplotlib.pyplot as plt
+import tensorflow.contrib.image
 import time as time
 import os
+from tqdm import tqdm
+from ChalearnLAPSample import GestureSample
+import math
 
 import util_testing
 
@@ -34,13 +38,15 @@ def exportPredictions(prediction_list, output_csv_file_path, seq_gestures, seq_p
 
 
 
-
-META_GRAPH_FILE = LOG_DIR + ''
+LOG_DIR = '/home/lc/Dynamic-Mario-Bros/src_ctc/runs/1497347590'
+META_GRAPH_FILE = 'model-6500.meta'
+MODEL_CP_PATH = '/home/lc/Dynamic-Mario-Bros/src_ctc/runs/1497347590'
+OUTPUT_PATH = '../evaluation/prediction/'''
 
 tf.flags.DEFINE_string('log_dir', LOG_DIR, 'Checkpoint directory')
 tf.flags.DEFINE_string('meta_graph_file', META_GRAPH_FILE, 'Name of meta graph file')
-tf.flags.DEFINE_string('model_checkpoint_path', LOG_DIR, 'Name of checkpoint file')
-tf.flags.DEFINE_interger('batch_size', BATCH_SIZE, 'Batch size')
+tf.flags.DEFINE_string('model_checkpoint_path', MODEL_CP_PATH, 'Name of checkpoint file')
+tf.flags.DEFINE_integer('batch_size', BATCH_SIZE, 'Batch size')
 
 FLAGS = tf.flags.FLAGS
 FLAGS._parse_flags()
@@ -49,66 +55,82 @@ for attr, value in sorted(FLAGS.__flags.items()):
     print('{}={}'.format(attr.upper(), value))
     print('')
 
-
-batch_samples_op, batch_sample_id_op, batch_clip_id_op, batch_padding_op = util_testing.input_pipeline(TEST_FILENAMES)
-
 prev_sample = -1
 prev_gesture= -1
 with tf.Session() as sess:
-    try:
-        saver = tf.train.import_meta_graph(os.path.join(FLAGS.log_dir, FLAGS.meta_graph_file))
-        saver.restore(sess, tf.train.latest_checkpoint(FLAGS.log_dir)
+    saver = tf.train.import_meta_graph(os.path.join(FLAGS.log_dir, FLAGS.meta_graph_file))
+    saver.restore(sess, tf.train.latest_checkpoint(FLAGS.log_dir))
 
-        predictions = tf.get_collections('predictions')[0]
-        input_samples_op = tf.get_collection('input_data')[0]
-        mode_op = tf.get_collection('mode')[0]
+    predictions = tf.get_collection('predictions')[0]
+    input_samples_op = tf.get_collection('input_samples_op')[0]
+    mode = tf.get_collection('mode')[0]
 
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(sess=sess,coord=coord)
+    for sample_id in tqdm(TEST_ID):
+        #print('========== sample %d ===========' % sample_id)
+        sample = GestureSample('%s/%s/Sample%04d.zip' % (RAW_DATA_PATH, 'Test', sample_id))
 
-        feed_dict = {mode:False, input_samples_op:batch_samples_op}
-        # or input_samples_op = batch_samples_op
-        request_output = [predictions, batch_sample_id_op, batch_clip_id_op, batch_padding_op]
-        predictions, sample_id, clip_id, padding = sess.run(requeset_output, feed_dict=feed_dict)
+        num_of_frames = sample.getNumFrames()
+        num_of_clip_batch = math.ceil(num_of_frames/FRAMES_PER_VIDEO/BATCH_SIZE)
+        # get entire video
+        user = sample.get_entire_user_video()
+        vid = sample.get_entire_rgb_video()
+        mask = np.mean(user, axis=3) > 150
+        mask = mask.reshape((mask.shape + (1,)))
+        vid = vid*mask
 
+        results = []
+        for clip_range in range(num_of_clip_batch):
+            start = clip_range * FRAMES_PER_CLIP * BATCH_SIZE
+            end = (clip_range + 1) * FRAMES_PER_CLIP * BATCH_SIZE
+            end_padding = 0
 
+            if end > num_of_frames:
+                end_padding = end - num_of_frames
+                end = num_of_frames
 
-        seq_gestures = []
-        seq_paddings = []
-        for i in range(len(predictions)):
+            batch_clips = np.asarray([list(vid[start:end]) + [vid[0]]*end_padding])
+            batch_clips = batch_clips[:,:, int((IMAGE_SIZE[0] - CROP[1])/2) : int((IMAGE_SIZE[0] + CROP[1])/2),
+                                           int((IMAGE_SIZE[1] - CROP[2])/2) : int((IMAGE_SIZE[1] + CROP[2])/2),:]
+            batch_clips = np.asarray(batch_clips, dtype=np.uint8).reshape((BATCH_SIZE, FRAMES_PER_CLIP) + (CROP[1], CROP[2], CROP[3]))
 
-            if (prev_sample != sample_id[i]):
-                if (prev_sample != -1):
-                    output_csv_file = os.path.join(prediction_dir,  subjectID + '_prediction.csv')
-                    exportPredictions(prediction_list, output_csv_file, seq_gestures, seq_paddings)
-                    #write to file
-                    seq_gestures = []
-                    seq_paddings = []
-
-                # The queue repeat
-                if (prev_sample > sample_id[i]):
-                    return 
-                prev_sample = sample_id[i]
-
-            if clip_id[i] != len(seq_gestures):
-                print("Clip ID is not in order!")
-                return
-
-            seq_gestures += [predictions[i]]
-            seq_paddings += [padding[i]]
-            
-
-    except Exception as e:
-        # Report exceptions to the coordinator.
-        print(str(e))
-        coord.request_stop(e)
-
-    finally:
-        # Terminate as usual. It is safe to call `coord.request_stop()` twice.
-        coord.request_stop()
-        coord.join(threads)
+            feed_dict = {mode:False, input_samples_op:batch_clips}
+            results += sess.run([predictions], feed_dict = feed_dict)[0].tolist()
 
 
-if __name__ == '__main__':
+        #print (results)
+        ''' write into files '''
+        prev_gesture = results[0]
+        start = 0
+        gestures = []
+        for i in range(len(results)):
+            if (results[i] != prev_gesture or (i+1) == len(results)):
 
-    tf.app.run()
+                start_frame = start * FRAMES_PER_CLIP + 1
+
+                if ((i+1) == len(results)):
+                    end_frame = min((i+1)*FRAMES_PER_CLIP, num_of_frames)
+                else:
+                    end_frame = min(i * FRAMES_PER_CLIP, num_of_frames)
+
+                if (prev_gesture+1 != NO_GESTURE):
+                    gestures += [[prev_gesture+1, start_frame, end_frame]]
+                    #gestures += [[prev_gesture, int((start_frame-1)/FRAMES_PER_CLIP), int((end_frame-1)/FRAMES_PER_CLIP)]]
+
+                prev_gesture = results[i]
+                start = i
+                if (i * FRAMES_PER_CLIP + 1 > num_of_frames):
+                    break
+
+        #print(results)
+        #print(gestures)
+        fout = open('%s/Sample%04d_prediction.csv' % (OUTPUT_PATH, sample_id), 'w')
+        for row in gestures:
+            fout.write(repr(int(row[0])) + ',' + repr(int(row[1])) + ',' + repr(int(row[2])))
+        fout.close()
+
+
+
+
+#if __name__ == '__main__':
+#
+#    tf.app.run()
