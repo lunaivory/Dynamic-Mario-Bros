8dd75696d44d8d320aea3ba890f267f4198448d2
Ubuntu <lc@GpuVM.ijfqyjbmupcutanud01fhshsle.ax.internal.cloudapp.net>
2017-06-16 19:57:54 +0000

    remove confusion in creating tfrecord

--- src_ctc/Raw_data_to_TFRecord.py
+++ src_ctc/Raw_data_to_TFRecord.py
@@ -3,9 +3,11 @@
 # 2. Shuffle TRAIN_ID, VALIDATION_ID, etc.
 
 # from training_data_to_tfrecords import get_data_training
-from training_data_to_tfrecords_3dcnn_last import get_data_training
+# from training_data_to_tfrecords_3dcnn_last import get_data_training
 #from val_data_to_tfrecords import get_data_val
 #from testing_data_to_tfrecords_3dcnn import get_data_testing
+import training_data_to_tfrecords_3dcnn
+import training_data_to_tfrecords_3dcnn_last
 
 '''############################'''
 '''# Self-defined library     #'''
@@ -15,6 +17,7 @@ from constants_3dcnn import *
 
 
 if __name__ == '__main__':
-    get_data_training(RAW_DATA_PATH, 'Train_3dcnn', TFRecord_DATA_PATH, TRAIN_ID)
+    training_data_to_tfrecords_3dcnn_last.get_data_training(RAW_DATA_PATH, 'Train_3dcnn', TFRecord_DATA_PATH, TRAIN_ID)
+    training_data_to_tfrecords_3dcnn.get_data_training(RAW_DATA_PATH, 'Train', TFRecord_DATA_PATH, TRAIN_ID)
     # get_data_val(RAW_DATA_PATH, 'Validation', TFRecord_DATA_PATH, VALIDATION_ID, label_path = RAW_DATA_PATH + 'Validation_reference/')
     # get_data_testing(RAW_DATA_PATH, 'Test', TFRecord_DATA_PATH, TEST_ID)
