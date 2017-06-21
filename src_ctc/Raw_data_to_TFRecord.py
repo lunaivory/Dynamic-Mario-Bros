b3ebbc490f16186ddbf404628b5ec22e49c38f7e
lunaivory <lc@GpuVM.ijfqyjbmupcutanud01fhshsle.ax.internal.cloudapp.net>
2017-06-21 22:38:11 +0000

    add balanced LSTM tfrecord creator

--- src_ctc/Raw_data_to_TFRecord.py
+++ src_ctc/Raw_data_to_TFRecord.py
@@ -13,11 +13,14 @@ import training_data_to_tfrecords_3dcnn_last
 '''# Self-defined library     #'''
 '''############################'''
 # from constants import *
-from constants_3dcnn import *
+# from constants_3dcnn import *
+import constants
+import constants_3dcnn
 
 
 if __name__ == '__main__':
-    training_data_to_tfrecords_3dcnn_last.get_data_training(RAW_DATA_PATH, 'Train_3dcnn', TFRecord_DATA_PATH, TRAIN_ID)
-    training_data_to_tfrecords_3dcnn.get_data_training(RAW_DATA_PATH, 'Train', TFRecord_DATA_PATH, TRAIN_ID)
+    #training_data_to_tfrecords_3dcnn_last.get_data_training(constants.RAW_DATA_PATH, 'Train_3dcnn', constants.TFRecord_DATA_PATH, constants_3dcnn.TRAIN_ID)
+    training_data_to_tfrecords_3dcnn.get_data_training(constants.RAW_DATA_PATH, 'Train', constants.TFRecord_DATA_PATH, constants.TRAIN_ID)
+    #training_data_to_tfrecords_3dcnn.get_data_training(RAW_DATA_PATH, 'Test', TFRecord_DATA_PATH, constants.TEST_ID)
     # get_data_val(RAW_DATA_PATH, 'Validation', TFRecord_DATA_PATH, VALIDATION_ID, label_path = RAW_DATA_PATH + 'Validation_reference/')
     # get_data_testing(RAW_DATA_PATH, 'Test', TFRecord_DATA_PATH, TEST_ID)
--- src_ctc/training_data_to_tfrecords_3dcnn.py
+++ src_ctc/training_data_to_tfrecords_3dcnn.py
@@ -19,7 +19,11 @@ from constants import *
 '''#  Create TFRecord  #'''
 '''#####################'''
 
+gesture_cnt = [0, 0]
+
 def get_data_training(path, data_type, write_path, sample_ids):
+
+    isTrain = data_type.find('Train') >= 0
 
     for sample_id in tqdm(sample_ids):
 
@@ -28,35 +32,9 @@ def get_data_training(path, data_type, write_path, sample_ids):
 
         '''Get label per frame'''
         gesture_list = sample.getGestures()
+            
         num_of_frames = sample.getNumFrames()
 
-        frames_range = list(np.arange(0, num_of_frames, FRAMES_PER_VIDEO_PP)[:-1])
-        frames_range.append(num_of_frames)
-
-        labels = []
-        ranges = []
-
-        dense_label = np.zeros(num_of_frames)
-        dense_label[:] = NO_GESTURE
-
-        for gesture_id, start_frame, end_frame in gesture_list:
-            labels += [gesture_id]
-            dense_label[(start_frame-1):end_frame] = gesture_id
-
-        #get also clip labels
-        clip_label_range = np.arange(0, num_of_frames, 8)
-        clip_labels = []
-
-        for clip_label in clip_label_range:
-            clip_dense_labels_slice = dense_label[clip_label: clip_label+8]
-            lab_truth = clip_dense_labels_slice != NO_GESTURE
-            n = np.sum(lab_truth)
-            if n > 5:
-                clip_labels.append(clip_dense_labels_slice[lab_truth][0])
-            else:
-                clip_labels.append(NO_GESTURE)
-
-        clip_labels = np.asarray(clip_labels, dtype=np.int32)
         # get entire video
         user = sample.get_entire_user_video()
         vid = sample.get_entire_rgb_video()
@@ -64,14 +42,58 @@ def get_data_training(path, data_type, write_path, sample_ids):
         mask = mask.reshape((mask.shape + (1,)))
         vid = vid*mask
 
+        labels = []
+
+        #get also clip labels
+        clip_label_range = np.arange(0, num_of_frames, FRAMES_PER_CLIP)
+        dense_label = np.zeros(num_of_frames)
+        dense_label[:] = NO_GESTURE
+        clip_labels = []
+
+        cut_dense_labels = []
+        cut_clip_labels = []
+        cut_vid = []
+
+        for gesture_id, start_frame, end_frame in gesture_list:
+            labels += [gesture_id]
+            dense_label[(start_frame-1):end_frame] = gesture_id
+
+        for clip_label in clip_label_range:
+            clip_dense_labels_slice = dense_label[clip_label: clip_label+FRAMES_PER_CLIP]
+            lab_truth = clip_dense_labels_slice != NO_GESTURE
+            n = np.sum(lab_truth)
+            if n > 5:
+                cut_clip_labels.append(clip_dense_labels_slice[lab_truth][0])
+                cut_vid.append(vid[clip_label : clip_label + FRAMES_PER_CLIP])
+                cut_dense_labels += [clip_dense_labels_slice[lab_truth][0]]*FRAMES_PER_CLIP
+                gesture_cnt[0] += 1
+            elif gesture_cnt[0] > 20 * gesture_cnt[1]:
+                cut_clip_labels.append(NO_GESTURE)
+                cut_vid.append(vid[clip_label : clip_label + FRAMES_PER_CLIP])
+                cut_dense_labels += [NO_GESTURE] * FRAMES_PER_CLIP
+                gesture_cnt[1] += 1
+
+        cut_clip_labels = np.asarray(cut_clip_labels, dtype=np.int32)
+        cut_vid = np.asarray(cut_vid, dtype = np.uint8)
+        cut_vid = np.reshape(cut_vid, (cut_vid.shape[0] * cut_vid.shape[1], cut_vid.shape[2], cut_vid.shape[3], cut_vid.shape[4]))
+        cut_dense_labels = np.asarray(cut_dense_labels, dtype=np.int32)
+
+
+        num_of_frames = cut_dense_labels.shape[0]
+
+        frames_range = list(np.arange(0, num_of_frames, FRAMES_PER_VIDEO_PP)[:-1])
+        frames_range.append(num_of_frames)
+
         for id in range(len(frames_range[:-1])):
             start = frames_range[id]
             end = frames_range[id+1]
+
+            clip_label_slice = np.asarray(cut_clip_labels[math.floor(start/8):math.floor(end/8)], dtype=np.int32)
             featureLists = tf.train.FeatureLists(feature_list={
-                'rgbs': util._bytes_feature_list(vid[start:end]),
-                'label': util._bytes_feature_list(np.asarray((clip_labels[id] - 1,), dtype=np.int32)),
-                'dense_label': util._bytes_feature_list(np.asarray(dense_label[start:end], dtype=np.int32) - 1),
-                'clip_label': util._bytes_feature_list(np.asarray(clip_labels[math.floor(start/8):math.floor(end/8)], dtype=np.int32) - 1),
+                'rgbs': util._bytes_feature_list(cut_vid[start:end]),
+                'label': util._bytes_feature_list(np.asarray((cut_clip_labels[id] - 1,), dtype=np.int32)),
+                'dense_label': util._bytes_feature_list(np.asarray(cut_dense_labels[start:end], dtype=np.int32) - 1),
+                'clip_label': util._bytes_feature_list(clip_label_slice- 1),
                 'sample_id': util._bytes_feature_list(np.asarray((sample_id,), dtype=np.int32)),
                 'num_frames': util._bytes_feature_list(np.asarray((num_of_frames,), dtype=np.int32))
             })
@@ -86,6 +108,8 @@ def get_data_training(path, data_type, write_path, sample_ids):
             tf_writer = tf.python_io.TFRecordWriter(filename, options=tf_write_option)
             tf_writer.write(sequence_example.SerializeToString())
             tf_writer.close()
+
+        print(gesture_cnt)
 
 
 
