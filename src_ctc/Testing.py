fa4817133de7a0d5abad38354710a95e46ede96f
lunaivory <lc@GpuVM.ijfqyjbmupcutanud01fhshsle.ax.internal.cloudapp.net>
2017-06-23 22:45:20 +0000

    modify test file for lstm predictions

--- src_ctc/Testing.py
+++ src_ctc/Testing.py
@@ -18,11 +18,11 @@ from constants import *
 from sklearn.metrics import jaccard_similarity_score
 
 
-# LOG_DIR = '/home/lc/Dynamic-Mario-Bros/src_ctc/runs/1497347590'
-LOG_DIR = '/home/federico/Dynamic-Mario-Bros/src_ctc/runs/1497471639/'
-META_GRAPH_FILE = 'model-15500.meta'
-# MODEL_CP_PATH = '/home/lc/Dynamic-Mario-Bros/src_ctc/runs/1497347590'
-MODEL_CP_PATH = '/home/federico/Dynamic-Mario-Bros/src_ctc/runs/1497471639/'
+LOG_DIR = '/home/lc/Dynamic-Mario-Bros/src_ctc/runs/1498164105'
+# LOG_DIR = '/home/federico/Dynamic-Mario-Bros/src_ctc/runs/1497471639/'
+META_GRAPH_FILE = 'modelLSTM-22000.meta'
+MODEL_CP_PATH = '/home/lc/Dynamic-Mario-Bros/src_ctc/runs/1498164105'
+# MODEL_CP_PATH = '/home/federico/Dynamic-Mario-Bros/src_ctc/runs/1498164105/'
 OUTPUT_PATH = '../evaluation/prediction/'
 REF_PATH = '../evaluation/reference/'
 
@@ -68,8 +68,11 @@ with tf.Session() as sess:
     saver.restore(sess, tf.train.latest_checkpoint(FLAGS.log_dir))
 
     predictions = tf.get_collection('predictions')[0]
+    predictions_lstm = tf.get_collection('predictions_lstm')[0]
     input_samples_op = tf.get_collection('input_samples_op')[0]
     mode = tf.get_collection('mode')[0]
+    mode_lstm = tf.get_collection('mode_lstm')[0]
+    net_type = tf.get_collection('net_type')[0]
 
     logits = tf.get_default_graph().get_tensor_by_name("accuracy/Reshape:0")
     logits_soft = tf.nn.softmax(logits)
@@ -91,16 +94,17 @@ with tf.Session() as sess:
         vid = vid*mask
 
         ''' get clip ground truth labels'''
-        clips_labels = []
-        dense_labels = [NO_GESTURE] * math.ceil(num_of_frames/FRAMES_PER_CLIP) * FRAMES_PER_CLIP
-        with open('%s/Sample%04d_labels.csv', 'r') as fin:
+        clip_labels = []
+        dense_labels = np.asarray([NO_GESTURE] * math.ceil(num_of_frames/FRAMES_PER_CLIP) * FRAMES_PER_CLIP)
+        with open('%s/Sample%04d_labels.csv' % ('../evaluation/reference', sample_id), 'r') as fin:
             for row in fin:
-                dense_labels[row[1]-1 : row[2]] = row[0]
+                row = row.split(',')
+                dense_labels[int(row[1])-1 : int(row[2])] = int(row[0])
 
         clip_label_range = np.arange(0, num_of_frames, 8)
 
         for clip_label in clip_label_range:
-            clip_dense_labels_slice = dense_label[clip_label: clip_label+8]
+            clip_dense_labels_slice = dense_labels[clip_label: clip_label+8]
             lab_truth = clip_dense_labels_slice != NO_GESTURE
             n = np.sum(lab_truth)
             if n > 5:
@@ -108,11 +112,11 @@ with tf.Session() as sess:
             else:
                 clip_labels.append(NO_GESTURE - 1)
 
-
         results = []
         for clip_range in range(int(num_of_clip_batch)):
-            start = clip_range * FRAMES_PER_CLIP * BATCH_SIZE
-            end = (clip_range + 1) * FRAMES_PER_CLIP * BATCH_SIZE
+            start = clip_range * FRAMES_PER_VIDEO 
+            end = (clip_range + 1) * FRAMES_PER_VIDEO 
+            print(start,end)
             end_padding = 0
 
             if end > num_of_frames:
@@ -122,12 +126,33 @@ with tf.Session() as sess:
             batch_clips = np.asarray([list(vid[start:end]) + [vid[0]]*end_padding])
             batch_clips = batch_clips[:,:, int((IMAGE_SIZE[0] - CROP[1])/2) : int((IMAGE_SIZE[0] + CROP[1])/2),
                                            int((IMAGE_SIZE[1] - CROP[2])/2) : int((IMAGE_SIZE[1] + CROP[2])/2),:]
-            batch_clips = np.asarray(batch_clips, dtype=np.float32).reshape((BATCH_SIZE, FRAMES_PER_CLIP) + (CROP[1], CROP[2], CROP[3]))
+            #print (batch_clips.shape)
+            batch_clips = np.asarray(batch_clips, dtype=np.float32).reshape((int(FRAMES_PER_VIDEO/FRAMES_PER_CLIP), FRAMES_PER_CLIP) + (CROP[1], CROP[2], CROP[3]))
+            print(batch_clips.shape)
+            print(batch_clips[0,7,50,50,1])
+            #print (batch_clips.shape)
 
-            feed_dict = {mode:False, input_samples_op:batch_clips}
-            _, logs = sess.run([predictions, logits_soft], feed_dict = feed_dict) #[0].tolist()
+            #TODO : add clip normalization
+            #batch_clips = tf.reshape(batch_clips, [CLIPS_PER_VIDEO, 1, FRAMES_PER_CLIP, CROP[1], CROP[2], CROP[3]])
+            #batch_clips_list = [tf.nn.batch_normalization(x,
+            #                                        mean = tf.nn.moments(x, axes=[0,1,2,3])[0],
+            #                                        variance = tf.nn.moments(x, axes=[0,1,2,3])[1],
+            #                                        offset = None, scale=None, variance_epsilon=1e-10)
+            #                    for x in tf.unstack(batch_clips)]
+            #batch_clips = tf.stack(batch_clips_list)
+            #batch_clips = tf.reshape(batch_clips, [CLIPS_PER_VIDEO, FRAMES_PER_CLIP, CROP[1], CROP[2], CROP[3]])
+            for batch_start in range(CLIPS_PER_VIDEO):
+                clip = batch_clips[batch_start]
+                mean = np.mean(clip)
+                var  = np.var(clip)
+                batch_clips[batch_start] = (clip - mean)/var
+
+
+            net_ty = True
+            feed_dict = {mode: False, mode_lstm: True, net_type: net_ty, input_samples_op:batch_clips}
+            preds, logs = sess.run([predictions_lstm, logits_soft], feed_dict = feed_dict) #[0].tolist()
             preds = np.argmax(logs, axis=1)
-            preds[np.max(logs, axis=1) < 0.4] = NO_GESTURE - 1
+            preds[np.max(logs, axis=1) < 0.2] = NO_GESTURE - 1
             results += preds.tolist()
 
         print (results)
@@ -138,7 +163,7 @@ with tf.Session() as sess:
             if (results[i]  == clip_labels[i]):
                 correct_predictions += 1
         total_predictions += len(clip_labels)
-        print ('Accuracy : %.1f %', correct_predictions / total_predictions * 100)
+        print ('Accuracy : %.1f ' % (correct_predictions / total_predictions * 100))
 
 
         ''' write into files '''
--- src_ctc/constants.py
+++ src_ctc/constants.py
@@ -3,9 +3,9 @@
 '''#####################'''
 
 # dont use sample 091 since there is a mistake with the labels
-TRAIN_ID = range(2, 10)         # Raw file format : Sample0001.zip - Sample0470.zip
+TRAIN_ID = range(1, 100)         # Raw file format : Sample0001.zip - Sample0470.zip
 VALIDATION_ID = range(475,478)  # Raw file format : Sample0471.zip - Sample0700.zip
-TEST_ID = range(701,751)        # Raw file format : Sample0701.zip - Sample0941.zip
+TEST_ID = range(701,703)        # Raw file format : Sample0701.zip - Sample0941.zip
 
 IMAGE_SIZE = (150, 120, 3)
 PADDING_SIZE = (1,) + IMAGE_SIZE
@@ -16,7 +16,7 @@ TFRecord_DATA_PATH = '../tf-data/'
 # use clips of 80 frames like they did in the paper
 FRAMES_PER_VIDEO_PP = 200 # FRAMES_PER_VIDEO_PP > FRAMES_PER_VIDEO
 FRAMES_PER_CLIP = 8 
-FRAMES_PER_VIDEO = 120 #80
+FRAMES_PER_VIDEO = 200 #80
 CLIPS_PER_VIDEO = int(FRAMES_PER_VIDEO_PP / FRAMES_PER_CLIP)
 
 """preprocesssing parameters"""
